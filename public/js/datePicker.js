b98eea6139e5d02688a3e962949451e84ba25825
Diego Herrera <dagomessis@gmail.com>
2022-10-10 12:13:50 -0400

    Updated UI

---
+++ public/js/datePicker.js
@@ -0,0 +1,14 @@
+mobiscroll.setOptions({
+    theme: 'ios',
+    themeVariant: 'light'
+});
+
+mobiscroll.datepicker('#demo', {
+    controls: ['calendar'],
+    display: 'inline'
+});
+
+mobiscroll.datepicker('#demo-timegrid', {
+    controls: ['calendar'],
+    display: 'inline'
+});
